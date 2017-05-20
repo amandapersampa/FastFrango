c8a70cde97d53201c10306640fa645aaa283e319
Amanda Pereira Sampaio <amandapersampa@hotmail.com>
2017-05-20 10:42:23 -0300

    primeiro build

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 from distutils.core import setup
 
 setup(
-    name="fastFrango",
-
-
+    name='fastFrango',
+    packages=['fastFrango'],
+    version='1.0.0',
+    description='Sistema para gerencia de um restaurante',
+    maintainer='Amanda Sampaio',
+    maintainer_email='amandapersampa@gmail.com',
+    classifiers=[
+        'Programing Language :: Python',
+        'Programing Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'License :: GNU Library',
+        'Operating System :: Ubuntu 16.0.5',
+        'Framework :: Flask'
+    ],
+    url='http://www.github.com/amandapersampa/FastFrango'
 )
