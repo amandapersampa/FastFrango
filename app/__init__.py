6fa87af6ba9007e3b4fefef891cca346858a6127
amandapersampa <amandapersampa@hotmail.com>
2017-06-04 22:35:58 -0300

    ajuste dos diretórios

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_cors import CORS, cross_origin
 import flask_restful
 
 app = Flask(__name__)
+CORS(app)
+
 app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
+
 manager.add_command('db', MigrateCommand)
 errors = {
     'sqlalchemy.exc.IntegrityError': {
@@ -22,7 +26,6 @@ errors = {
     },
 }
 api = flask_restful.Api(app, errors=errors)
-
 
 from app.dao.Produto_dao import Produto_dao
 from app.dao.Compra_dao import Compra_dao
